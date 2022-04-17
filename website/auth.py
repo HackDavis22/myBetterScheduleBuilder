b0d6071a33566f111d374aa185712c2a0fe9bc3a
O-Gee-San <bdgee925@gmail.com>
2022-04-17 10:21:12 -0700

    q

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from logic import *
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('selection')
+@auth.route('selection', methods = ('GET', 'POST'))
 def selection():
+
     return render_template("selection.html")
